fdd56c5e0803fb18d6b66c5a61cf72eb9113da63
camma <christoph.amma@kinemic.de>
2023-09-04 23:25:30 +0200

    dav-co2-accounting-1 add all categories

--- co2data/categories/importer.py
+++ co2data/categories/importer.py
@@ -1,18 +1,37 @@
 import csv
+from pathlib import Path
 from typing import TextIO, List
 
 from co2data.categories.category import Category
+from co2data.categories.category_provider import CategoryProvider
+from co2data.categories.concrete_category_provider import ConcreteCategoryProvider
+
+
+def create_category_provider_from_directory(directory: Path) -> CategoryProvider:
+    categories = []
+    for csv_file in directory.glob("*.csv"):
+        with open(csv_file, "r", encoding="utf-8") as filehandle:
+            categories.extend(import_csv(filehandle))
+    return ConcreteCategoryProvider(categories)
 
 
 def import_csv(csv_file: TextIO) -> List[Category]:
-    """Create a CategoryProvider instance initialized with all categories given in csv file"""
-    csv_name = 'DATENPUNKT'
-    csv_description = 'BESCHREIBUNG'
-    csv_example = 'BEISPIEL'
-    csv_unit = 'EINHEIT'
+    """
+    Read a list of categories from a csv file.
+
+    The csv file is expected to have the column headers 'Datenpunkt', 'Beschreibung', 'Beispiel' and 'Einheit' in the
+    first row.
+    The delimiter must be a semicolon.
+
+    :param csv_file: TextIO object to read data from
+    """
+    csv_name = 'Datenpunkt'
+    csv_description = 'Beschreibung'
+    csv_example = 'Beispiel'
+    csv_unit = 'Einheit'
     category_reader = csv.DictReader(csv_file, delimiter=';')
     categories = []
     for row in category_reader:
         if row[csv_name] != '':
-            categories.append(Category(row[csv_name], row[csv_unit], row[csv_description]))
+            categories.append(Category(row[csv_name], row[csv_example], row[csv_unit], row[csv_description]))
     return categories
